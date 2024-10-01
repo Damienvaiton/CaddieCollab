e1a58fa5eb9eeed49fb1b5ca0b4ce57b2c700a23
VAITON DAMIEN p2106425 <damien.vaiton@etu.univ-lyon1.fr>
2024-10-01 12:20:10 +0200

    First exemple of nav working

--- App.tsx
+++ App.tsx
@@ -1,13 +1,54 @@
 import * as React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+};
+
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+type RootStackParamList = {
+	Home: undefined;
+	Details: undefined;
+};
+
+type HomeScreenNavigationProp = NativeStackNavigationProp<
+	RootStackParamList,
+	"Home"
+>;
+
+function HomeScreen({ navigation }: { navigation: HomeScreenNavigationProp }) {
+	return (
+		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+			<Text>Home Screen</Text>
+			<Button
+				title="Go to Details"
+				onPress={() => navigation.navigate("Details")}
+			/>
+		</View>
+	);
+}
+
+function DetailsScreen() {
+	return (
+		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+			<Text>Details Screen</Text>
+		</View>
+	);
+}
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
-		</View>
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Details" component={DetailsScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
 	);
 }
 
