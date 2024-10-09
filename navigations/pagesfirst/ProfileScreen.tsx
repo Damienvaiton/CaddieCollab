7dce92e83e77abd446cc0e1790b5fd5ea96c0517
VAITON DAMIEN p2106425 <damien.vaiton@etu.univ-lyon1.fr>
2024-10-09 15:41:59 +0200

    Update Android resources and dependencies

--- navigations/pagesfirst/ProfileScreen.tsx
+++ navigations/pagesfirst/ProfileScreen.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Button, StyleSheet, Text, View, Image } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 
+import auth from "@react-native-firebase/auth";
+
 import styles from "../../styles/DefaultStyles";
 
 type RootStackParamList = {
@@ -38,9 +40,29 @@ export default function ProfileScreen({
 			),
 		});
 	});
+
+	const Register = () => {
+		auth()
+			.createUserWithEmailAndPassword("test1@test.test", "testtest")
+			.then(() => {
+				console.log("User account created & signed in!");
+			})
+			.catch((error) => {
+				if (error.code === "auth/email-already-in-use") {
+					console.log("That email address is already in use!");
+				}
+
+				if (error.code === "auth/invalid-email") {
+					console.log("That email address is invalid!");
+				}
+
+				console.error(error);
+			});
+	};
+
 	return (
 		<View style={styles.container}>
-			<Text>Profile Screen</Text>
+			<Button title="Register" onPress={Register} />
 		</View>
 	);
 }
